0cb1401afc6951ab86135a49f63d5ca56146af16
VaiSuliafu <52583173+VaiSuliafu@users.noreply.github.com>
2020-07-27 07:36:51 -0500

    Computer Solution & Problem Application

---
+++ Least Squares/CP4.1-1.Suliafu.Vai.main.m
@@ -0,0 +1,20 @@
+function [x, error] = LS(A, b)
+  AT_A = transpose(A) * A;
+  AT_b = transpose(A) * b;
+  x = inv(AT_A) * AT_b;
+  preds = A * x;
+  residuals = b - preds;
+  error = norm(residuals,2);
+endfunction
+
+%Part A
+A = [3,-1,2; 4,1,0; -3,2,1; 1,1,5; -2,0,3];
+b = [10; 10; -5; 15; 0];
+[solX_PartA, resid_norm_PartA] = LS(A, b)
+
+fprintf("\n");
+
+%Part B 
+A = [4,2,3,0; -2,3,-1,1; 1,3,-4,2; 1,0,1,-1; 3,1,3,-2];
+b = [10; 0; 2; 0; 5];
+[solX_PartB, resid_norm_PartB] = LS(A, b)
---
+++ Least Squares/CP4.3-1.Suliafu.Vai.main.m
@@ -0,0 +1,37 @@
+% Program 4.1 Modified Gram-Schmidt Orthogonalization
+% Input: m x n matrix A with linearly independent columns
+% Output: Orthogonal matrix Q, upper triangular R
+% Example usage: [Q,R]=clgs(A)
+function [Q,R] = clgs(A)
+  [m,n] = size(A);
+  for j = 1:n
+    y = A(:,j);
+    for i = 1:j-1
+      R(i,j) = Q(:,i)'* y;
+      y = y - R(i,j) * Q(:,i);
+    endfor
+    R(j,j) = norm(y);
+    Q(:,j) = y/R(j,j);
+  endfor
+endfunction
+
+%PartA
+fprintf("Part A\n")
+[Q, R] = clgs([4,0; 3,1])
+[Q_octave, R_octave] = qr([4,0; 3,1])
+fprintf("\n");
+
+fprintf("Part B\n")
+[Q, R] = clgs([1,2; 1, 1])
+[Q_octave, R_octave] = qr([1,2; 1, 1])
+fprintf("\n");
+
+fprintf("Part C\n")
+[Q, R] = clgs([2,1; 1,-1; 2,1])
+[Q_octave, R_octave] = qr([2,1; 1,-1; 2,1])
+fprintf("\n");
+
+fprintf("Part D\n")
+[Q, R] = clgs([4, 8, 1; 0, 2, -2; 3, 6, 7])
+[Q_octave, R_octave] = qr([4, 8, 1; 0, 2, -2; 3, 6, 7])
+fprintf("\n");
